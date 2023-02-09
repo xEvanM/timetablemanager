5e12cfb706222d1395d3b7b49cdefe4d0a0364f3
Evan <evanmurphy2003@gmail.com>
2023-02-09 23:23:21 +0000

    Functions updated

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,16 +1,11 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const cors = require('cors')({origin:true});
-admin.initializeApp();
+const cors = require('cors')({ origin: true });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-  exports.addModule = functions.https.onRequest((request, response) => {
-    cors(request, response, async () => {
-      // your code to add a document to Firestore
-      const res = await db.collection('modules').doc('testing1').set(data);
-      response.send(res);
-    });
+exports.addModule = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    // your code to add a document to Firestore
+    const res = await db.collection('modules').doc('testing1').set(data);
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.send(res);
   });
+});
