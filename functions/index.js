a8aa66c3cb71bb3ca28c16750c1b3663cb433e7d
Michael McC <67327140+mccurtinator@users.noreply.github.com>
2023-03-01 22:50:26 +0000

    Added framework to enhance Lecturer addModule function
    Created an alternative Lecturer page to implement new module-adding code
    
    Also created the corresponding Firebase function

--- functions/index.js
+++ functions/index.js
@@ -10,3 +10,31 @@ exports.addModule = functions.https.onRequest((request, response) => {
         });
     });
 });
+
+
+// enhanced addModule that gets data on students and lectuers
+
+// Add a module to Firestore
+exports.addModule2 = functions.https.onCall(async (data, context) => {
+
+    // TODO: export data from lecturer page to here
+  const { location, name, students, taughtby, times } = data;
+
+
+  const db = admin.firestore();
+
+  try {
+    // Retrieve all students who are studying this module
+    const studentsSnapshot = await db.collection("students").where("studies", "array-contains", name).get();
+    const studentRefs = studentsSnapshot.docs.map((doc) => db.collection("students").doc(doc.id));
+
+    // Add the module data to Firestore
+    const moduleData = { location, name, students: studentRefs, taughtby: db.collection("lecturers").doc(taughtby), times };
+    const moduleRef = await db.collection("modules").add(moduleData);
+    
+    return { success: true, id: moduleRef.id };
+  } catch (error) {
+    console.error("Error adding module:", error);
+    return { success: false };
+  }
+});
