ad1d3e1631d58a298ebfcfabbdaaf14abb44e782
johnstew324 <j.stewart12@nuigalway.ie>
2023-03-08 17:15:10 +0000

    added daily view page

---
+++ src/views/Dailyview.vue
@@ -0,0 +1,40 @@
+<template>
+    <head>
+        <title>
+            DailyView
+        </title>
+    </head>
+
+    <body>
+        <div class="loginimg">
+            <img src=https://cdn-icons-png.flaticon.com/512/277/277991.png>
+        </div>
+        <div class="background"></div>
+        <span class="twelcome">Daily View for, User!</span>
+    </body>
+</template>
+<script>
+</script>
+
+<style scoped> .background {
+     background: rgba(21, 38, 70, 1);
+     height: 1080px;
+     width: 1920px;
+     position: absolute;
+     right: 800px;
+     top: 0px;
+     left: 0px;
+ }
+ .twelcome {
+    width: 538px;
+    color: rgba(255, 255, 255, 1);
+    position: absolute;
+    top: 49px;
+    left: 132px;
+    font-family: Inter;
+    font-weight: Bold;
+    font-size: 70px;
+    opacity: 1;
+    text-align: left;
+}
+</style>
