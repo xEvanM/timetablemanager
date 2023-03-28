9ece55545f4413da576fadf5d4846a9a086877e0
Evan M <evanmurphy2003@gmail.com>
2023-03-28 01:01:34 +0100

    Authentication implemented for student routes and lecturer routes - correct access level required

--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,74 @@ import { createRouter, createWebHistory } from 'vue-router';
 import firebase from '../api/firebase.js'
 import {getAuth, onAuthStateChanged, signOut} from "firebase/auth" ;
 import app from '../api/firebase.js';
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+const functions = getFunctions(app);
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/login", component: () => import("../views/Login.vue") },
         { path: "/signup", component: () => import("../views/SignUp.vue") },
-        { path: "/student", component: () => import("../views/Student.vue"), beforeEnter: isAuth  },
         { path: "/forgot", component: () => import("../views/ForgotPassword.vue") },
-        { path: "/lecturer", component: () => import("../views/Lecturer.vue") },
-        { path: "/adminreg", component: () => import("../views/LecturerSignup.vue") },
-        { path: "/daily", component: () => import("../views/Daily.vue"), beforeEnter: isAuth  }
+        { path: "/student", component: () => import("../views/Student.vue"), beforeEnter: isAuthStudent},
+        { path: "/lecturer", component: () => import("../views/Lecturer.vue"), beforeEnter: isAuthLecturer },
+        { path: "/daily", component: () => import("../views/Daily.vue"), beforeEnter: isAuthStudent },
     ]
 });
 
-function isAuth(to, from, next) {
-    console.log("Checking auth");
+function isAuthLecturer(to, from, next) {
     const auth = getAuth(app);
+    console.log("Checking if user is a lecturer");
+
     onAuthStateChanged(auth, (user) => {
-        if (user) {
+        if(user) {
             console.log(user);
-            // user is signed in so continue to page requested 
-            return next();
+            const getAccessLevel = httpsCallable(functions, "getAccessLevel");
+            const email = auth.currentUser.email;
+            const data = {
+                email: email,
+            };
+            getAccessLevel(data).then((result) => {
+                console.log("Result: " + result.data);
+                if (result.data === "lecturer") {
+                    console.log("User is a lecturer");
+                    return next();
+                } else {
+                    console.log("User is not a lecturer");
+                    return next({ path: "/login" });
+                }
+            });
         } else {
-            // if the user is signed out, send to login page
-            return next({path: '/login'});
+            return next({ path: "/login" })
+        }
+    });
+}
+
+function isAuthStudent(to, from, next) {
+    const auth = getAuth(app);
+    console.log("Checking if user is a student");
+
+    onAuthStateChanged(auth, (user) => {
+        if(user) {
+            console.log(user);
+            const getAccessLevel = httpsCallable(functions, "getAccessLevel");
+            const email = auth.currentUser.email;
+            const data = {
+                email: email,
+            };
+            getAccessLevel(data).then((result) => {
+                console.log("Result: " + result.data);
+                if (result.data === "student") {
+                    console.log("User is a student");
+                    return next();
+                } else {
+                    console.log("User is not a student");
+                    return next({ path: "/login" });
+                }
+            });
+        } else {
+            return next({ path: "/login" })
         }
     });
 }
